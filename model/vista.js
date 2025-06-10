828558322793b63d47dd59f2968c8bd861f47a70
JoacoA13 <joaquin.astorino@gmail.com>
2025-06-09 21:46:57 -0300

    [2.2.2] se avanza con unificacion de css y themes

--- model/vista.js
+++ model/vista.js
@@ -22,9 +22,34 @@ export class Vista {
         this.pagCarrito ={
             divCarrito: this.$("carrito")
         }
+
+        this.botonTheme = this.$("boton-theme");
+        this.initTheme();
     }
 
     $(x) {
         return document.getElementById(x);
+    }
+
+    initTheme() {
+        const body = document.body;
+        const temas = ['modoClaro', 'modoOscuro', 'modoJPI'];
+        let temaActual = localStorage.getItem('theme') || 'modoClaro';
+
+        body.classList.add(temaActual);
+
+        if (this.botonTheme) {
+            this.botonTheme.addEventListener('click', () => {
+                body.classList.remove(temaActual);
+                let nextIndex = (temas.indexOf(temaActual) + 1) % temas.length;
+                temaActual = temas[nextIndex];
+                body.classList.add(temaActual);
+                localStorage.setItem('theme', temaActual);
+            });
+        }
+    }
+
+    setearLogo(){
+        
     }
 }
